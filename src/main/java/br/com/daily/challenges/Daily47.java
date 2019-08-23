a12a7aab7259e227c78d7edf60c2fe96babc4979
Gustavo Bezerra <gustavobz305@gmail.com>
2019-08-22 21:17:20 -0300

    Adding Daily Challenge #47

---
+++ src/main/java/br/com/daily/challenges/Daily47.java
@@ -0,0 +1,34 @@
+package br.com.daily.challenges;
+
+import java.util.Scanner;
+
+/**
+ * <a>https://dev.to/thepracticaldev/daily-challenge-47-alphabets-4cbn</a>
+ * @author Gustavo
+ * @date 2019-08-22
+ *
+ */
+public class Daily47 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String phrase = scanner.nextLine();
+		
+		System.out.println(convertToNumbers(phrase));
+		scanner.close();
+	}
+
+	public static String convertToNumbers(String phrase) {
+		StringBuilder sb = new StringBuilder();
+		if(phrase != null) {
+			for(char letter : phrase.toUpperCase().toCharArray()) {
+				if(letter >= 65 && letter <= 90) {
+					sb.append(letter - 64);
+					sb.append(" ");
+				}
+			}
+		}
+		return sb.substring(0, sb.length()-1).toString();
+	}
+
+}
---
+++ src/test/java/br/com/daily/challenges/Daily47Test.java
@@ -0,0 +1,15 @@
+package br.com.daily.challenges;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Daily47Test {
+	
+	@Test
+	public void resolutionExpected() {
+		String converted = Daily47.convertToNumbers("The sunset sets at twelve o' clock.");
+		assertEquals("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11", converted);
+	}
+
+}
